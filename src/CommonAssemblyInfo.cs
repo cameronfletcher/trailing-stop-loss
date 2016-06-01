fa564a529af5b0c42dcd6315328687ee9c31ed3d
Cameron Fletcher <cameron.fletcher@cameronfletcher.com>
2016-06-01 10:39:56 +0300

    Initial commit

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="CommonAssemblyInfo.cs" company="Skeleton contributors">
+//  Copyright (c) Skeleton contributors. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skeleton contributors")]
+[assembly: AssemblyCopyright("Copyright © Skeleton contributors. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyProduct("Skeleton")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-alpha")]
+
+[assembly: SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly", Justification = "Using SemVer.")]
